c70ae6bcd3266f873e2b495efa933727675e187c
anniken2 <144472300+anniken2@users.noreply.github.com>
2023-11-11 18:39:46 +0100

    versjon1
    Første versjon av tørr vits

---
+++ app.js
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+
+console.log(
+  chalk.blue("Har ") +
+    chalk.red("du ") +
+    chalk.yellow("hørt ") +
+    chalk.green("om ") +
+    chalk.cyan("han ") +
+    chalk.blue("som ") +
+    chalk.magenta("ikke ") +
+    chalk.grey("kom ") +
+    chalk.blue("på ") +
+    chalk.greenBright("hva ") +
+    chalk.redBright("støv ") +
+    chalk.blue("betød ") +
+    chalk.black("på ") +
+    chalk.yellowBright("engelsk? ") +
+    chalk.blue(" - Han ") +
+    chalk.cyan("følte ") +
+    chalk.magenta("seg ") +
+    chalk.green("ganske ") +
+    chalk.blue("dust...")
+);
